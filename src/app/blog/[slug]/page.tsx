c12bb9cfd2488736742794f3ab8fd54b376dc60c
hieuddo <hieu.dd.1998@gmail.com>
2026-06-03 21:51:47 +0800

    fix: featuredImage max-width

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -41,6 +41,7 @@ export default async function PostPage({
             src={post.featuredImage}
             alt={post.title}
             fill
+            sizes="(max-width: 1024px) 100vw, 896px"
             className="object-cover"
             priority
           />
